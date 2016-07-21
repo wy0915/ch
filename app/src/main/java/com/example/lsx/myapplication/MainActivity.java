13ac839eef3877f96efd57e4c21adce670866201
as <12@ex.com>
2016-07-21 16:15:19 +0800

    第二次修改

--- app/src/main/java/com/example/lsx/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lsx/myapplication/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.lsx.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button=(Button) findViewById(R.id.activity_main_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("WY","UROIWUQROIWEUQOI");
+            }
+        });
     }
+
 }
