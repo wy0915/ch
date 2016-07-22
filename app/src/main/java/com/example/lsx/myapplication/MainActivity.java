9ae72b749cba9908029e7ff749e688a8ec4fe628
as <12@ex.com>
2016-07-22 09:01:47 +0800

    单击按钮添加文本

--- app/src/main/java/com/example/lsx/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lsx/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,10 +14,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button=(Button) findViewById(R.id.activity_main_button);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d("WY","UROIWUQROIWEUQOI");
+                 EditText eidt= (EditText) findViewById(R.id.j123);
+                eidt.setText("单击按钮添加文本");
             }
         });
     }
